813a64b9f177d8e38de251258db81eb6d6448d11
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-22 17:37:23 +0400

    initialized sqqlite database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"events-api/db"
 	"events-api/models"
 	"net/http"
 
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
